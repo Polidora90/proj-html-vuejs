764bdfb158d8d67ee42e2247678bf3db299fa2c4
Polidora90 <alessandra.torry@gmail.com>
2021-05-25 17:15:43 +0200

    inizio Vue

---
+++ script/app.js
@@ -0,0 +1,31 @@
+new Vue({
+    el: "#app",
+    data: {
+        navbarLinks: [
+            {
+                text: "HOME", //sistemare maiuscolo tramite js
+                link: "#" 
+            },
+            {
+                text: "ABOUT",
+                link: "#" 
+            },
+            {
+                text: "SERVICE",
+                link: "#" 
+            },
+            {
+                text: "SHOWCASE",
+                link: "#" 
+            },
+            {
+                text: "BLOG",
+                link: "#" 
+            },
+            {
+                text: "CONTACT",
+                link: "#" 
+            }
+        ]
+    }
+});

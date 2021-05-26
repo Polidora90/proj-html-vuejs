2db25eae080bd453b29815822fe7603245ecd66e
Polidora90 <alessandra.torry@gmail.com>
2021-05-26 17:08:12 +0200

    vue submit mail

--- script/app.js
+++ script/app.js
@@ -53,6 +53,18 @@ new Vue({
                 text: "Contact",
                 link: "#"
             }
-        ]
+        ],
+        customerMail: ""
+    },
+    methods: {
+        registerMail() {
+            if(!this.customerMail || !this.customerMail.includes("@")) {
+                alert("La tua mail non sembra corretta. Devi inserire almeno un carattere ed utilizzare il simbolo @")
+            } else {
+                alert("Benvenuto! Controlla la tua casella di posta, ti abbiamo inviato una mail!")
+            }
+
+            this.customerMail = "";
+        }
     }
 });
